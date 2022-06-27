5bd0c97a65e4f78b72469db6880988cf1c80bdfe
onjsdnjs <onjsdnjs@gmail.com>
2022-06-27 16:14:24 +0900

    Spring Security Fundamentals - SecurityBuilder / SecurityConfigurer

--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityOauth2Application.class, args);
     }
