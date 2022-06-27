d84c636d6372c03bebf334d298ddc2e723275351
onjsdnjs <onjsdnjs@gmail.com>
2022-06-27 15:45:38 +0900

    Initial commit

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
