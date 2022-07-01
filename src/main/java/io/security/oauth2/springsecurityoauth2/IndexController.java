d672773c4a432586e387a25c929f240ac2be3b50
onjsdnjs <onjsdnjs@gmail.com>
2022-07-02 08:28:18 +0900

    ClientRegistration

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -0,0 +1,20 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @GetMapping("/")
+    public String index() {
+        ClientRegistration clientRegistration = this.clientRegistrationRepository.findByRegistrationId("keycloak");
+        String clientName = clientRegistration.getClientName();
+        return clientName;
+    }
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -19,10 +19,10 @@ public class OAuth2ClientConfig {
         private ClientRegistration keycloakClientRegistration() {
             return ClientRegistration.withRegistrationId("keycloak")
                     .clientId("oauth2-client-app")
-                    .clientSecret("dddddddd")
+                    .clientSecret("CQueEWXZYmv7IIZVxbvh2uwxptXVaRcX")
                     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    .redirectUri("http://localhost:8081/login/oauth2/code/{registrationId}")
+                    .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
                     .scope("openid", "profile", "email", "address", "phone")
                     .authorizationUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/auth")
                     .tokenUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/token")
