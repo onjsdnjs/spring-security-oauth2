6a5697e83c933972b3de45089990e13d25583714
onjsdnjs <onjsdnjs@gmail.com>
2022-07-01 22:34:13 +0900

    ClientRegistration

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -7,30 +7,29 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 
 @Configuration
 public class OAuth2ClientConfig {
 
         @Bean
         public ClientRegistrationRepository clientRegistrationRepository() {
-            return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+            return new InMemoryClientRegistrationRepository(this.keycloakClientRegistration());
         }
 
-        private ClientRegistration googleClientRegistration() {
-            return ClientRegistration.withRegistrationId("google")
-                    .clientId("google-client-id")
-                    .clientSecret("google-client-secret")
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+        private ClientRegistration keycloakClientRegistration() {
+            return ClientRegistration.withRegistrationId("keycloak")
+                    .clientId("oauth2-client-app")
+                    .clientSecret("dddddddd")
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+                    .redirectUri("http://localhost:8081/login/oauth2/code/{registrationId}")
                     .scope("openid", "profile", "email", "address", "phone")
-                    .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-                    .tokenUri("https://www.googleapis.com/oauth2/v4/token")
-                    .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-                    .userNameAttributeName(IdTokenClaimNames.SUB)
-                    .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-                    .clientName("Google")
+                    .authorizationUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/auth")
+                    .tokenUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/token")
+                    .userInfoUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/userinfo")
+                    .userNameAttributeName("preferred_username")
+                    .jwkSetUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/certs")
+                    .clientName("Keycloak")
                     .build();
         }
 }
