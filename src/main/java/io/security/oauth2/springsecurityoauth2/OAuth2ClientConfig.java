12acb5211a3ca0bf461d8c02812a472eeafde0dd
onjsdnjs <onjsdnjs@gmail.com>
2022-07-02 09:29:30 +0900

    OAuth2LoginConfigurer

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -2,36 +2,18 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
-
-        @Bean
-        public ClientRegistrationRepository clientRegistrationRepository() {
-            return new InMemoryClientRegistrationRepository(this.keycloakClientRegistration());
-        }
-
-        private ClientRegistration keycloakClientRegistration() {
-            return ClientRegistration.withRegistrationId("keycloak")
-                    .clientId("oauth2-client-app")
-                    .clientSecret("CQueEWXZYmv7IIZVxbvh2uwxptXVaRcX")
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                    .scope("openid", "profile", "email", "address", "phone")
-                    .authorizationUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/auth")
-                    .tokenUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/token")
-                    .userInfoUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/userinfo")
-                    .userNameAttributeName("preferred_username")
-                    .jwkSetUri("http://localhost:8080/realms/oauth2/protocol/openid-connect/certs")
-                    .clientName("Keycloak")
-                    .build();
-        }
+    @Bean
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client();
+        return http.build();
+   }
 
 }
