5fc68c57a644c127d5b33802224874d09a5ce1a1
onjsdnjs <onjsdnjs@gmail.com>
2022-07-03 22:54:22 +0900

    OpenID Connect 로그아웃

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,7 +1,13 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,27 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/user")
+    public OAuth2User user(Authentication authentication){
+        OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
+        OAuth2User oAuth2User2 = oAuth2AuthenticationToken.getPrincipal();
+        System.out.println("oAuth2User = " + oAuth2User);
+        System.out.println("oAuth2User2 = " + oAuth2User2);
+        return oAuth2User;
+    }
+
+    @GetMapping("/oauth2User")
+    public OAuth2User oauth2User(@AuthenticationPrincipal OAuth2User oAuth2User){
+        System.out.println("oAuth2User = " + oAuth2User);
+        return oAuth2User;
+    }
+
+    @GetMapping("/oidcUser")
+    public OidcUser oidcUser(@AuthenticationPrincipal OidcUser oidcUser){
+        System.out.println("oidcUser = " + oidcUser);
+        return oidcUser;
     }
 }
