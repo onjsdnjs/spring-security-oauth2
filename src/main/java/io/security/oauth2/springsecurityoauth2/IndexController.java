89e6de07f1db927d569932057614fb7fff4e3c2d
onjsdnjs <onjsdnjs@gmail.com>
2022-07-03 22:20:13 +0900

    OpenID Connect 로그아웃

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -0,0 +1,15 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
