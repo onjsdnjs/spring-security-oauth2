d68d76ada9e8330b1afe9cf1be642c4638188868
onjsdnjs <onjsdnjs@gmail.com>
2022-07-03 22:36:56 +0900

    OpenID Connect 로그아웃

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -12,25 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
         http.oauth2Login(Customizer.withDefaults());
-        http.logout()
-                .logoutSuccessHandler(oidcLogoutSuccessHandler())
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID");
 
         return http.build();
-    }
-
-    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-        return successHandler;
     }
 }
