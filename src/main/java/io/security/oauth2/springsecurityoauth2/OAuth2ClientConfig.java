f94202a6cdadfd8b299c77677869b06a1f7c44b3
onjsdnjs <onjsdnjs@gmail.com>
2022-07-04 00:24:45 +0900

    OAuth2ClientConfigurer 초기화 및 설정

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,45 +1,20 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.function.Consumer;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
         http
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(authorization -> authorization
-                                .authorizationRequestResolver(authorizationRequestResolver(clientRegistrationRepository))
-                        )
-                );
-
+                .oauth2Login().and()
+                .oauth2Client()
+                ;
         return http.build();
     }
-    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository) {
-        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
-                new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
-        authorizationRequestResolver.setAuthorizationRequestCustomizer(authorizationRequestCustomizer());
-        return authorizationRequestResolver;
-    }
-
-    private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
-        return customizer -> customizer.authorizationRequestUri(uriBuilder -> uriBuilder.queryParam("customName", "1234").build());
-    }
-
-
 }
