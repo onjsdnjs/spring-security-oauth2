cd7dda8773657774bc2ad3eb15140352ca5d6214
onjsdnjs <onjsdnjs@gmail.com>
2022-07-03 23:39:21 +0900

    API 커스텀 설정 Endpoint BaseUrl

--- src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
@@ -1,13 +1,14 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class IndexController {
+@Controller
+public class LoginController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -14,7 +14,7 @@ public class OAuth2ClientConfig {
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeRequests((requests) -> requests.antMatchers("/login").permitAll().anyRequest().authenticated());
         http
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
