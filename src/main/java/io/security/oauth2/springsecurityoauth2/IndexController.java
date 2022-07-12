f323efb84ddaba5b10fa8f9c7b854ca0680f79bf
onjsdnjs <onjsdnjs@gmail.com>
2022-07-12 18:40:51 +0900

    OAuth 2.0 Resource Server Fundamentals - application.properties / OAuth2ResourceServerProperties

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,6 +1,7 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
