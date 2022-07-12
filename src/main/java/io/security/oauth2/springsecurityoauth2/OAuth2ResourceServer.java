ad7decb4c7fd94e66ad6eca91062129a23621ad0
onjsdnjs@gmail.com <nl01180623!!>
2022-07-12 21:50:55 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
@@ -1,13 +1,22 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServer {
+
+    @Autowired
+    private OAuth2ResourceServerProperties properties;
 
     @Bean
     SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -15,5 +24,14 @@ public class OAuth2ResourceServer {
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
+
+    /*@Bean
+    public JwtDecoder jwtDecoder1() {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+    }*/
+
+    @Bean
+    public JwtDecoder jwtDecoder2() {
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).jwsAlgorithm(SignatureAlgorithm.RS512).build();}
 
 }
