cb16a2b67ba134b3b2070b64ca8c893e6bfc1a2d
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 21:11:17 +0900

    OAuth2 MAC & RSA 검증 - MAC 검증

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
@@ -1,8 +1,28 @@
 package io.security.oauth2.springsecurityoauth2.configs;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SignatureConfig {
+    @Bean
+    public MacSecuritySigner macSecuritySigner() {
+        return new MacSecuritySigner();
+    }
+    @Bean
+    public OctetSequenceKey octetSequenceKey() throws JOSEException {
+        OctetSequenceKey octetSequenceKey = new OctetSequenceKeyGenerator(256)
+                .keyID("macKey")
+                .algorithm(JWSAlgorithm.HS256)
+                .generate();
+        return octetSequenceKey;
+    }
 
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/signature/SecuritySigner.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/signature/SecuritySigner.java
@@ -1,5 +1,32 @@
 package io.security.oauth2.springsecurityoauth2.signature;
 
-public abstract class SecuritySigner {
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Date;
+
+public abstract class SecuritySigner {
+    public String getJwtTokenInternal(JWSSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject("user")
+                .issuer("http://localhost:8081")
+                .claim("username", user.getUsername())
+                .claim("authority", user.getAuthorities())
+                .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 5))
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build(), jwtClaimsSet);
+        signedJWT.sign(jwsSigner);
+        String jwtToken = signedJWT.serialize();
+
+        return jwtToken;
+    }
+    protected abstract String getJwtToken(UserDetails user, JWK jwk) throws JOSEException;
 }
