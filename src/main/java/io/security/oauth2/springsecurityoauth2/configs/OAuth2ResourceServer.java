6aaf39b130c8d34c10579570e0498c77822d86ba
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 11:35:11 +0900

    기본 환경 구성

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -8,11 +8,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServer {
-
     @Bean
     SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll().anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.formLogin().permitAll();
         return http.build();
     }
+
 }
