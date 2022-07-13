84b5b2417d12fcf2705781e3387a18e234f194d4
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 22:35:02 +0900

    OAuth2 MAC & RSA 검증 - RSA 검증

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
@@ -2,14 +2,9 @@ package io.security.oauth2.springsecurityoauth2.configs;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
 import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
-import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
 import io.security.oauth2.springsecurityoauth2.signature.RSASecuritySigner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,7 +16,7 @@ public class SignatureConfig {
     }
     @Bean
     public RSAKey rsaKey() throws JOSEException {
-        RSAKey rsaKey = new RSAKeyGenerator(256)
+        RSAKey rsaKey = new RSAKeyGenerator(2048)
                 .keyID("rsaKey")
                 .algorithm(JWSAlgorithm.RS256)
                 .generate();
