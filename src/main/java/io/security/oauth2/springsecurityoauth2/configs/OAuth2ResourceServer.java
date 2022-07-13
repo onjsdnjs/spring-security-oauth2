abddd5514375d6953f042a62c134c55e6ac50da1
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 20:26:13 +0900

    OAuth2 MAC & RSA 검증 - 기본환경 및 공통클래스 구성

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -1,14 +1,22 @@
 package io.security.oauth2.springsecurityoauth2.configs;
 
+import io.security.oauth2.springsecurityoauth2.filter.authentication.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Arrays;
 
@@ -16,9 +24,13 @@ import java.util.Arrays;
 public class OAuth2ResourceServer {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll().anyRequest().authenticated());
+
+        http.csrf().disable();
+
+        http.authorizeRequests((requests) -> requests.antMatchers("/login").permitAll().anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         http.userDetailsService(getUserDetailsService());
+        http.addFilterBefore(new JwtAuthenticationFilter(http), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
@@ -1,42 +1,33 @@
 package io.security.oauth2.springsecurityoauth2.filter.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.KeyLengthException;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import io.security.oauth2.springsecurityoauth2.dto.LoginDto;
-import io.security.oauth2.springsecurityoauth2.signature.SecuritySigner;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private AuthenticationManager authenticationManager;
-    private SecuritySigner securitySigner;
-    private JWK jwk;
+    private HttpSecurity httpSecurity;
 
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, SecuritySigner securitySigner, JWK jwk) {
-
-        this.authenticationManager = authenticationManager;
-        this.securitySigner = securitySigner;
-        this.jwk = jwk;
+    public JwtAuthenticationFilter(HttpSecurity httpSecurity) {
+        this.httpSecurity = httpSecurity;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        AuthenticationManager authenticationManager = httpSecurity.getSharedObject(AuthenticationManager.class);
+
         ObjectMapper objectMapper = new ObjectMapper();
         LoginDto loginDto = null;
         try {
@@ -48,27 +39,11 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
         return authentication;
     }
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
-
-        UserDetails user = (UserDetails)authResult.getPrincipal();
-        String jwtToken;
-        try {
-            MACSigner jwsSigner = new MACSigner(((OctetSequenceKey)jwk).toSecretKey());
-            jwtToken = securitySigner.getJwtToken(jwsSigner, user, jwk);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        Map<String, Object> detailMap = new HashMap<>();
-        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authResult;
-        WebAuthenticationDetails details = (WebAuthenticationDetails)token.getDetails();
-
-        detailMap.put("detail", details);
-        detailMap.put("Authorization", "Bearer " + jwtToken);
-        token.setDetails(detailMap);
-
-        response.addHeader("Authorization", "Bearer " + jwtToken);
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws ServletException, IOException {
+        getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
     }
 }
