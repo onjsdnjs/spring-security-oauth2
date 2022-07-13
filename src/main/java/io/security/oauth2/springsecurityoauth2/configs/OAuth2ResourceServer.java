a5559a298020aca19a5f898a2c0674b3bc1c9cab
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 17:31:40 +0900

    OAuth2 MAC & RSA 검증 - 기본환경 및 공통클래스 구성

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -7,9 +7,14 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServer {
@@ -18,7 +23,12 @@ public class OAuth2ResourceServer {
         http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll().anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         http.formLogin().permitAll();
-        http.authenticationProvider(new DaoAuthenticationProvider());
+
+        User user = new User("user", "{noop}1234", Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(user));
+        http.authenticationProvider(daoAuthenticationProvider);
 
         return http.build();
     }
