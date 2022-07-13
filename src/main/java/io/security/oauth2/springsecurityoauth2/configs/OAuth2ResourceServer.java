660ada09306f30530a4150822d112f7217f39900
onjsdnjs@gmail.com <nl01180623!!>
2022-07-13 17:17:06 +0900

    OAuth2 MAC & RSA 검증 - 기본환경 및 공통클래스 구성

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -2,17 +2,23 @@ package io.security.oauth2.springsecurityoauth2.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServer {
     @Bean
-    SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll().anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         http.formLogin().permitAll();
+        http.authenticationProvider(new DaoAuthenticationProvider());
 
         return http.build();
     }
