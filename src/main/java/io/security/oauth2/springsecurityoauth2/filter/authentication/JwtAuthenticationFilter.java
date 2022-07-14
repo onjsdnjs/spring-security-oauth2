94c86cac4e5bceeea21b1bc3d13d45349ee4349e
onjsdnjs <onjsdnjs@gmail.com>
2022-07-14 21:59:20 +0900

    RSA 검증 기능 구현 - JwtAuthorizationMacFilter

--- src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
@@ -3,27 +3,21 @@ package io.security.oauth2.springsecurityoauth2.filter.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import io.security.oauth2.springsecurityoauth2.dto.LoginDto;
-import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
 import io.security.oauth2.springsecurityoauth2.signature.SecuritySigner;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
--- src/main/java/io/security/oauth2/springsecurityoauth2/filter/authorization/JwtAuthorizationRsaFilter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authorization/JwtAuthorizationRsaFilter.java
@@ -60,5 +60,4 @@ public class JwtAuthorizationRsaFilter extends OncePerRequestFilter {
 
         chain.doFilter(request, response);
     }
-
 }
