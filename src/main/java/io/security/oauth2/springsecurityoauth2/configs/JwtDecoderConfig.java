6da2c5db89267fa0a5133a1830e7edc991844903
onjsdnjs <onjsdnjs@gmail.com>
2022-07-14 22:27:48 +0900

    RSA 검증 기능 구현 - JwtDecoder

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/JwtDecoderConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/JwtDecoderConfig.java
@@ -1,11 +1,14 @@
 package io.security.oauth2.springsecurityoauth2.configs;
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.RSAKey;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
@@ -13,7 +16,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 public class JwtDecoderConfig {
 
     @Bean
-    @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithm", havingValue = "HS256", matchIfMissing = false)
+    @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithms", havingValue = "HS256", matchIfMissing = false)
     public JwtDecoder jwtDecoderBySecretKeyValue(OctetSequenceKey octetSequenceKey,OAuth2ResourceServerProperties properties) {
         return NimbusJwtDecoder.withSecretKey(octetSequenceKey.toSecretKey())
                 .macAlgorithm(MacAlgorithm.from(properties.getJwt().getJwsAlgorithms().get(0)))
@@ -21,10 +24,10 @@ public class JwtDecoderConfig {
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithm", havingValue = "RS512", matchIfMissing = false)
-    public JwtDecoder jwtDecoderByPublicKeyValue(OctetSequenceKey octetSequenceKey,OAuth2ResourceServerProperties properties) {
-        return NimbusJwtDecoder.withSecretKey(octetSequenceKey.toSecretKey())
-                .macAlgorithm(MacAlgorithm.from(properties.getJwt().getJwsAlgorithms().get(0)))
+    @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithms", havingValue = "RS512", matchIfMissing = false)
+    public JwtDecoder jwtDecoderByPublicKeyValue(RSAKey rsaKey, OAuth2ResourceServerProperties properties) throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey())
+                .signatureAlgorithm(SignatureAlgorithm.from(properties.getJwt().getJwsAlgorithms().get(0)))
                 .build();
     }
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/SignatureConfig.java
@@ -3,8 +3,11 @@ package io.security.oauth2.springsecurityoauth2.configs;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
+import io.security.oauth2.springsecurityoauth2.signature.RSASecuritySigner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,4 +28,16 @@ public class SignatureConfig {
         return octetSequenceKey;
     }
 
+    @Bean
+    public RSASecuritySigner rsaSecuritySigner() {
+        return new RSASecuritySigner();
+    }
+    @Bean
+    public RSAKey rsaKey() throws JOSEException {
+        RSAKey rsaKey = new RSAKeyGenerator(2048)
+                .keyID("rsaKey")
+                .algorithm(JWSAlgorithm.RS512)
+                .generate();
+        return rsaKey;
+    }
 }
