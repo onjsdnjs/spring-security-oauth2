a2f971da193ac97fbc4ea8a92f4ffb4dc9fcd602
onjsdnjs <onjsdnjs@gmail.com>
2022-07-14 21:07:52 +0900

    MAC 검증 기능 구현 - SecretKey 에 의한 검증

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/JwtDecoderConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/JwtDecoderConfig.java
@@ -16,7 +16,7 @@ public class JwtDecoderConfig {
     @ConditionalOnProperty(prefix = "spring.security.oauth2.resourceserver.jwt", name = "jws-algorithm", havingValue = "HS256", matchIfMissing = false)
     public JwtDecoder jwtDecoderBySecretKeyValue(OctetSequenceKey octetSequenceKey,OAuth2ResourceServerProperties properties) {
         return NimbusJwtDecoder.withSecretKey(octetSequenceKey.toSecretKey())
-                .macAlgorithm(MacAlgorithm.from(properties.getJwt().getJwsAlgorithm()))
+                .macAlgorithm(MacAlgorithm.from(properties.getJwt().getJwsAlgorithms().get(0)))
                 .build();
     }
 }
