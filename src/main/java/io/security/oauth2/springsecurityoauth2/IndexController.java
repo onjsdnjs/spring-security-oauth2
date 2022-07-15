6bb55616c23b6f98f6398657e24f64533620d26b
onjsdnjs <onjsdnjs@gmail.com>
2022-07-16 01:03:06 +0900

    Authentication & @AuthenticationPrincipal

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -3,7 +3,9 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public BearerTokenAuthentication index(Authentication authentication, @AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal){
+    public JwtAuthenticationToken index(Authentication authentication, @AuthenticationPrincipal Jwt principal){
 
-        BearerTokenAuthentication bearerTokenAuthentication = (BearerTokenAuthentication) authentication;
-        String sub = bearerTokenAuthentication.getTokenAttributes().get("sub") + " is the subject";
-        String sub1 = principal.getAttribute("sub") + " is the subject";
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
+        String sub = jwtAuthenticationToken.getTokenAttributes().get("sub") + " is the subject";
+        String sub1 = principal.getClaim("sub") + " is the subject";
 
-        return bearerTokenAuthentication;
+        return jwtAuthenticationToken;
     }
 }
