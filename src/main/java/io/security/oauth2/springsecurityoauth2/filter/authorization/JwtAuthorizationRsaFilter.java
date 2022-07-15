f59067da49693f1976d2bd2e093992c8fd7dd198
onjsdnjs <onjsdnjs@gmail.com>
2022-07-15 22:41:11 +0900

    RSA 검증 기능 구현 - JwtDecoder

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authorization/JwtAuthorizationRsaFilter.java
@@ -0,0 +1,46 @@
+package io.security.oauth2.springsecurityoauth2.filter.authorization;
+
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class JwtAuthorizationRsaFilter extends JwtAuthorizationFilter {
+
+    public JwtAuthorizationRsaFilter(RSAKey jwk, JWSVerifier jwsVerifier) {
+		super(jwk, jwsVerifier);
+    }
+
+	public JwtAuthorizationRsaFilter(JWK jwk, JwtDecoder jwtDecoder) {
+		super(jwk, jwtDecoder);
+
+	}
+
+	@Override
+	protected void executeDecoding(HttpServletRequest request, JWSVerifier jwsVerifier, String token) {
+
+		if(jwtDecoder != null) {
+			Jwt jwt = jwtDecoder.decode(token);
+			String username = jwt.getClaimAsString("username");
+			List<String> authority = jwt.getClaimAsStringList("authority");
+
+			if (username != null) {
+				UserDetails user = User.builder().username(username)
+						.password("")
+						.authorities(authority.get(0))
+						.build();
+				Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		}
+	}
+}
