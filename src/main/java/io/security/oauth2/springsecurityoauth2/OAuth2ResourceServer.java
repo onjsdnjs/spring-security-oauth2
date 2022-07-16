b2b35b82018f0ad986a2851f35421cf00d4c71c4
onjsdnjs <onjsdnjs@gmail.com>
2022-07-16 20:15:24 +0900

    리소스 서버 권한 구현 - Scope 기반 권한 설정

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
@@ -2,6 +2,7 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +11,9 @@ public class OAuth2ResourceServer {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeRequests((requests) -> requests.antMatchers(HttpMethod.GET, "/photos/**")
+                .hasAuthority("SCOPE_photo")
+                .anyRequest().authenticated());
         http.oauth2ResourceServer().jwt();
         return http.build();
     }
