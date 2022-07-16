54d6f35d666edb9723d2fa0d100f469a4687e580
onjsdnjs <onjsdnjs@gmail.com>
2022-07-16 22:32:31 +0900

    리소스 서버 권한 구현 - 권한 설정 커스터마이징- JwtAuthenticationConverter

--- src/main/java/io/security/oauth2/springsecurityoauth2/CustomRoleConverter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomRoleConverter.java
@@ -10,29 +10,22 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CustomRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
     public Collection<GrantedAuthority> convert(final Jwt jwt) {
-  
-        Map<String, Object> realmAccess = (Map<String, Object>)jwt.getClaims().get("realm_access");
 
-        if (realmAccess == null || realmAccess.isEmpty()) {
+        String scopes = (String)jwt.getClaims().get("scope");
+
+        if (scopes == null || scopes.isEmpty()) {
             return new ArrayList<>();
         }
 
-        Collection<GrantedAuthority> returnValue = ((List<String>)realmAccess.get("roles"))
-                .stream().map(roleName -> "ROLE_" + roleName)  
+        return Arrays.stream(scopes.split(" ")).map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
- 
-        return returnValue;
     }
-
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
@@ -14,16 +15,22 @@ public class OAuth2ResourceServer {
 
     @Bean
     SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomRoleConverter());
+
         http.antMatcher("/photos/1").authorizeRequests(
                 (requests) -> requests.antMatchers(HttpMethod.GET, "/photos/1")
-                .hasAuthority("SCOPE_photo")
+//                .hasAuthority("SCOPE_photo")
+                .hasAuthority("ROLE_photo")
                 .anyRequest().authenticated());
-        http.oauth2ResourceServer().jwt();
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
         return http.build();
     }
 
     @Bean
     SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+
         http.antMatcher("/photos/2").authorizeRequests(
                 (requests) -> requests.antMatchers(HttpMethod.GET, "/photos/2").permitAll());
         http.oauth2ResourceServer().jwt();
