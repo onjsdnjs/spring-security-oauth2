f3b73840411aa4cade6dbfe81d79cce2f5337fdf
onjsdnjs <onjsdnjs@gmail.com>
2022-07-17 22:14:55 +0900

    OAuth 2.0 Client + OAuth 2.0 Resource Server 연동

---
+++ ResourceServer/src/main/java/io/resourceserver/resourceserver/IndexController.java
@@ -0,0 +1,13 @@
+package io.resourceserver.resourceserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
