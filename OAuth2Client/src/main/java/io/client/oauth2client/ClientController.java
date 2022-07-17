c9a71d334e8f89e81d6b4770facfd03ad33a1980
onjsdnjs <onjsdnjs@gmail.com>
2022-07-17 23:10:36 +0900

    OAuth 2.0 Client + OAuth 2.0 Resource Server 연동

--- OAuth2Client/src/main/java/io/client/oauth2client/ClientController.java
+++ OAuth2Client/src/main/java/io/client/oauth2client/ClientController.java
@@ -1,20 +1,13 @@
 package io.client.oauth2client;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
 
-    @GetMapping("/client")
-    public String client(Model model, @RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient oAuth2AuthorizedClient){
-
-        OAuth2AccessToken accessToken = oAuth2AuthorizedClient.getAccessToken();
-        model.addAttribute("access token", accessToken.getTokenValue());
+    @GetMapping("/photos")
+    public String client(String accessToken){
 
         return "client";
     }
--- OAuth2Client/src/main/java/io/client/oauth2client/IndexController.java
+++ OAuth2Client/src/main/java/io/client/oauth2client/IndexController.java
@@ -1,5 +1,9 @@
 package io.client.oauth2client;
 
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model, @RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient oAuth2AuthorizedClient){
+
+        OAuth2AccessToken accessToken = oAuth2AuthorizedClient.getAccessToken();
+        model.addAttribute("access token", accessToken.getTokenValue());
+
         return "index";
     }
 }
