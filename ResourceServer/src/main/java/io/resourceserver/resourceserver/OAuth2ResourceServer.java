e39f957356fefbfed07f3541e38c0814d6f20ba1
onjsdnjs <onjsdnjs@gmail.com>
2022-07-17 22:12:40 +0900

    OAuth 2.0 Client + OAuth 2.0 Resource Server 연동

---
+++ ResourceServer/src/main/java/io/resourceserver/resourceserver/OAuth2ResourceServer.java
@@ -0,0 +1,21 @@
+package io.resourceserver.resourceserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class OAuth2ResourceServer {
+
+    @Bean
+    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(
+                (requests) -> requests.antMatchers("/photos").access("SCOPE_photo")
+                        .anyRequest().authenticated());
+        http.oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
