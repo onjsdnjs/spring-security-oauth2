2cf40ef31bcac495c2621fd506a65bc71abdc3e2
onjsdnjs <onjsdnjs@gmail.com>
2022-07-19 00:22:48 +0900

    OAuth 2.0 Authorization Server 소개

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,7 +1,5 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
