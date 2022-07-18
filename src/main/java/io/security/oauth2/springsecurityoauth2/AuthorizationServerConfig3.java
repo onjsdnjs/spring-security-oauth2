52486b0dcc3f8825a0aea016708d9a5c32860c3f
onjsdnjs <onjsdnjs@gmail.com>
2022-07-19 01:01:56 +0900

    OAuth 2.0 Authorization Server 소개

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/AuthorizationServerConfig3.java
@@ -0,0 +1,71 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig3 {
+
+    @Bean
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer<>();
+        http.apply(authorizationServerConfigurer);
+        authorizationServerConfigurer
+				.registeredClientRepository(registeredClientRepository)
+				.authorizationService(authorizationService)
+				.authorizationConsentService(authorizationConsentService)
+				.providerSettings(providerSettings)
+				.tokenGenerator(tokenGenerator)
+				.clientAuthentication(clientAuthentication -> {})
+				.authorizationEndpoint(authorizationEndpoint -> {})
+				.tokenEndpoint(tokenEndpoint -> {})
+				.tokenIntrospectionEndpoint(tokenIntrospectionEndpoint -> {})
+				.tokenRevocationEndpoint(tokenRevocationEndpoint -> {})
+				.oidc(oidc -> oidc
+						.userInfoEndpoint(userInfoEndpoint -> {})
+						.clientRegistrationEndpoint(clientRegistrationEndpoint -> {}));
+        return http.build();
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder().issuer("http://localhost:9000").build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oauth2-client-app")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8081/login/oauth2/code/oauth2-client-app")
+                .redirectUri("http://127.0.0.1:8081")
+                .scope(OidcScopes.OPENID)
+                .scope("message.read")
+                .scope("message.write")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        InMemoryRegisteredClientRepository registeredClientRepository = new InMemoryRegisteredClientRepository(registeredClient);
+        return registeredClientRepository;
+    }
+}
