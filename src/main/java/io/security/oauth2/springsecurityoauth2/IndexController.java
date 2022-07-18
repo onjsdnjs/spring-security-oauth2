47d6cd055a4e53013d5229cd6ca9a6b36e79bb9f
onjsdnjs <onjsdnjs@gmail.com>
2022-07-18 23:37:41 +0900

    OAuth 2.0 Authorization Server 소개

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
     @GetMapping("/")
     public String index(){
         return "index";
--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -2,7 +2,6 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
     public static void main(String[] args) {
