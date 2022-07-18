e89dd33c6e28b8ca56a0171823ac38980742bcbb
onjsdnjs <onjsdnjs@gmail.com>
2022-07-18 09:47:50 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,11 +1,9 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
