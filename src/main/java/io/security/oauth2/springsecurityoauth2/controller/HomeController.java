ae2218c5f0a4ab25ccb1b43d7a206e81f42d28ae
onjsdnjs <onjsdnjs@gmail.com>
2022-07-28 23:57:29 +0900

    SocialLogin-Google_연동

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
@@ -27,6 +27,7 @@ public class HomeController {
 
     @GetMapping("/oidc") // 요청시 scope 에 openid 가 포함되어야 oidcUser 가 생성된다
     public String oidc(Model model, Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
+        System.out.println("oidcUser = " + oidcUser);
         model.addAttribute("oAuth2User",oidcUser);
         model.addAttribute("authentication",authentication);
         return "home";
