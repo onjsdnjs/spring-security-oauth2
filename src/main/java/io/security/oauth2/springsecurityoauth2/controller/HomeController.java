0a84185ae072194757fc4efbda46f7ff66c5a5a7
onjsdnjs <onjsdnjs@gmail.com>
2022-07-28 22:26:30 +0900

    SocialLogin-Google_연동

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
@@ -1,6 +1,11 @@
 package io.security.oauth2.springsecurityoauth2.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +15,19 @@ public class HomeController {
 
     @GetMapping("/user")
     public String user(Model model, Authentication authentication) {
+
+        OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+        System.out.println("oAuth2User = " + oAuth2User);
+
+        model.addAttribute("oAuth2User",oAuth2User);
         model.addAttribute("authentication",authentication);
+
         return "home";
     }
 
     @GetMapping("/oidc")
-    public String oidc(Model model, Authentication authentication) {
+    public String oidc(Model model, Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
+        model.addAttribute("oAuth2User",oidcUser);
         model.addAttribute("authentication",authentication);
         return "home";
     }
