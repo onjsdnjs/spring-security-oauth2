095cede6786c434f4b729a7f36bbf34e20277827
onjsdnjs <onjsdnjs@gmail.com>
2022-07-29 00:23:51 +0900

    SocialLogin-Google_연동

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeRestController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeRestController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeRestController {
 
-    @GetMapping("/user")
+    @GetMapping("/api/user")
     public Authentication user(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2User) {
         System.out.println("authentication = " + authentication + ", oAuth2User = " + oAuth2User);
         return authentication;
     }
 
-    @GetMapping("/oidc") // 요청시 scope 에 openid 가 포함되어야 oidcUser 가 생성된다
+    @GetMapping("/api/oidc") // 요청시 scope 에 openid 가 포함되어야 oidcUser 가 생성된다
     public Authentication oidc(Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
         System.out.println("authentication = " + authentication + ", oidcUser = " + oidcUser);
         return authentication;
