a367a806a6ccf93934190c8aee53ed3c0d41bf57
onjsdnjs <onjsdnjs@gmail.com>
2022-07-29 00:23:12 +0900

    SocialLogin-Google_연동

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
@@ -18,8 +18,6 @@ public class HomeController {
 
         OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
         System.out.println("oAuth2User = " + oAuth2User);
-
-        model.addAttribute("oAuth2User",oAuth2User);
         model.addAttribute("authentication",authentication);
 
         return "home";
@@ -28,7 +26,6 @@ public class HomeController {
     @GetMapping("/oidc") // 요청시 scope 에 openid 가 포함되어야 oidcUser 가 생성된다
     public String oidc(Model model, Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
         System.out.println("oidcUser = " + oidcUser);
-        model.addAttribute("oAuth2User",oidcUser);
         model.addAttribute("authentication",authentication);
         return "home";
     }
---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeRestController.java
@@ -0,0 +1,26 @@
+package io.security.oauth2.springsecurityoauth2.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+
+    @GetMapping("/user")
+    public Authentication user(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2User) {
+        System.out.println("authentication = " + authentication + ", oAuth2User = " + oAuth2User);
+        return authentication;
+    }
+
+    @GetMapping("/oidc") // 요청시 scope 에 openid 가 포함되어야 oidcUser 가 생성된다
+    public Authentication oidc(Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
+        System.out.println("authentication = " + authentication + ", oidcUser = " + oidcUser);
+        return authentication;
+    }
+}
