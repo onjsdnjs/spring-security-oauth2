b9fe4102787008230d871ae6bd1b78c74e204009
onjsdnjs <onjsdnjs@gmail.com>
2022-07-31 09:42:26 +0900

    JCA_&_JCE-소개_및_구조

--- src/main/java/io/security/oauth2/springsecurityoauth2/MacTest.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/MacTest.java
@@ -1,4 +1,27 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class MacTest {
+
+    public static String hmac(String data) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+
+        String secret = "hmacKey";
+        String algorithms = "HmacSHA256";
+
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("utf-8"), algorithms);
+
+        Mac mac = Mac.getInstance(algorithms);
+
+        mac.init(secretKey);
+
+        byte[] hash = mac.doFinal(data.getBytes());
+
+        return Base64.getEncoder().encodeToString(hash);
+    }
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -11,6 +11,7 @@ public class SpringSecurityOauth2Application {
     public static void main(String[] args) throws Exception {
 
 //        messageDigest("Spring Security");
+//        hmac("Spring Security");
 //        signature("Spring Security");
         rsa("Spring Security");
 
