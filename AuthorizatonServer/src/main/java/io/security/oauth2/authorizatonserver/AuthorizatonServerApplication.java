807a46517439de00c7f42e51bbbcbb1f7d807661
onjsdnjs <onjsdnjs@gmail.com>
2022-08-01 15:37:56 +0900

    OAuth_2.0_Client_+_OAuth_2.0_Resource_Server_연동_+_Authorization Server 연동

---
+++ AuthorizatonServer/src/main/java/io/security/oauth2/authorizatonserver/AuthorizatonServerApplication.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.authorizatonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizatonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizatonServerApplication.class, args);
+    }
+
+}
---
+++ AuthorizatonServer/src/test/java/io/security/oauth2/authorizatonserver/AuthorizatonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.authorizatonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizatonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
