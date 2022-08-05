7f30b085b3e348573e58ec714e6cc5daa2596fbd
onjsdnjs <onjsdnjs@gmail.com>
2022-08-05 14:20:14 +0900

    동의하기 커스텀 구현

--- AuthorizatonServer/src/main/java/io/security/oauth2/authorizatonserver/AuthorizationServerApplication.java
+++ AuthorizatonServer/src/main/java/io/security/oauth2/authorizatonserver/AuthorizationServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
     }
