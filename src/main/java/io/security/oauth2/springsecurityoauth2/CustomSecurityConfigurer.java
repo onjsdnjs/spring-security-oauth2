e73bbe2a91af213f43a7f7ef67e121a12fc9b5b9
onjsdnjs <onjsdnjs@gmail.com>
2022-08-08 19:21:07 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
@@ -1,6 +1,5 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -11,10 +10,17 @@ public class CustomSecurityConfigurer
     @Override
     public void init(HttpSecurity builder) throws Exception {
         super.init(builder);
+        System.out.println("init method starting..");
     }
     @Override
     public void configure(HttpSecurity builder) throws Exception {
         super.configure(builder);
+        if(isSecure){
+            System.out.println("https is required");
+        }else{
+            System.out.println("https is optional");
+        }
+        System.out.println("configure method starting..");
     }
     public CustomSecurityConfigurer setFlag(boolean isSecure){
         this.isSecure = isSecure;
