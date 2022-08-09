61c5f3100ba8a8f76a1a4e480a7bf731ad3883d9
onjsdnjs <onjsdnjs@gmail.com>
2022-08-09 22:58:39 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/CustomAuthenticationEntryPoint.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,6 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
+        System.out.println("제일 우선적으로 실행이 됩니다.");
     }
 }
