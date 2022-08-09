545196743c472a4f367da45c01ebba598c73fcb7
onjsdnjs <onjsdnjs@gmail.com>
2022-08-09 19:32:31 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -3,15 +3,17 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig{
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer()).setFlag(true);
+        http.apply(new CustomSecurityConfigurer()).setFlag(false);
         return http.build();
     }
 }
