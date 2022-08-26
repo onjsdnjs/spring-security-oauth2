46c02039cd90c0dc589d96d7868f427727568403
onjsdnjs <onjsdnjs@gmail.com>
2022-08-27 01:05:17 +0900

    lecture

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
