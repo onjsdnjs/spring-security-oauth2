0bebe274d036772eefcd0773e8cf3c3da6a3569f
onjsdnjs <onjsdnjs@gmail.com>
2022-08-29 18:13:21 +0900

    Consent

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/HomeController.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,20 +1,34 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
 //        http.authorizeRequests((requests) -> requests.antMatchers("/login").permitAll().anyRequest().authenticated());
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
-        http.oauth2Login(Customizer.withDefaults());
+        http.authorizeRequests((requests) -> requests
+                .antMatchers("/home")
+                .permitAll()
+                .anyRequest().authenticated());
+        http.oauth2Login(oauth2Login -> oauth2Login.authorizationEndpoint(authEndpoint -> authEndpoint.authorizationRequestResolver(customResolver())));
 //        http.oauth2Login(oauth2 -> oauth2.loginPage("/login"));
         return http.build();
    }
+
+    private OAuth2AuthorizationRequestResolver customResolver() {
+        return new CustomResolver(clientRegistrationRepository, OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
+    }
 }
