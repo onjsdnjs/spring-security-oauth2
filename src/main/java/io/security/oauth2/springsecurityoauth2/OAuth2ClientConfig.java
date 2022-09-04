e82e901c6934ecccfc2bf74cd6a19762286984f0
onjsdnjs <onjsdnjs@gmail.com>
2022-09-04 18:49:06 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,19 +1,20 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
 public class OAuth2ClientConfig {
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
         http
-                .oauth2Login().and()
-                .oauth2Client()
+                .oauth2Login(Customizer.withDefaults())
+                //.oauth2Client(Customizer.withDefaults())
                 ;
         return http.build();
     }
