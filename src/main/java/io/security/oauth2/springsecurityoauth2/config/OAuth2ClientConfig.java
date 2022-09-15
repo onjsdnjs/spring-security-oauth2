9505eeec832874e682d60b74ca176b818db69926
onjsdnjs <onjsdnjs@gmail.com>
2022-09-16 00:44:57 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,13 +1,23 @@
 package io.security.oauth2.springsecurityoauth2.config;
 
+import io.security.oauth2.springsecurityoauth2.filter.CustomOAuth2LoginAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
+
+    @Autowired
+    private DefaultOAuth2AuthorizedClientManager authorizedClientManager;
+    @Autowired
+    private OAuth2AuthorizedClientRepository authorizedClientRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +27,16 @@ public class OAuth2ClientConfig {
         http
 //                .oauth2Login(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults());
+        http.addFilterBefore(customOAuth2LoginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    public CustomOAuth2LoginAuthenticationFilter customOAuth2LoginAuthenticationFilter() throws Exception {
+        CustomOAuth2LoginAuthenticationFilter customOAuth2LoginAuthenticationFilter =
+                new CustomOAuth2LoginAuthenticationFilter(authorizedClientManager,authorizedClientRepository);
+        customOAuth2LoginAuthenticationFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
+            response.sendRedirect("/home");
+        });
+        return customOAuth2LoginAuthenticationFilter;
     }
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/HomeController.java
@@ -1,13 +1,39 @@
 package io.security.oauth2.springsecurityoauth2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken){
+
+        OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService
+                .loadAuthorizedClient("keycloak", oAuth2AuthenticationToken.getName());
+
+        model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken);
+        model.addAttribute("AccessToken", authorizedClient.getAccessToken().getTokenValue());
+        model.addAttribute("RefreshToken", authorizedClient.getRefreshToken().getTokenValue());
+
         return "home";
     }
 }
