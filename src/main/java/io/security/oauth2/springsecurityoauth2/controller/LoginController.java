5956174ecb6660c6bbf15297ce5722d7cf11ce2e
onjsdnjs <onjsdnjs@gmail.com>
2022-09-15 15:44:35 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/LoginController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
@@ -17,7 +16,6 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -26,8 +24,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 @Controller
@@ -51,47 +47,45 @@ public class LoginController {
                 .attribute(HttpServletResponse.class.getName(), response)
                 .build();
 
-        OAuth2AuthorizationSuccessHandler successHandler = (authorizedClient, principal, attributes) ->
-                oAuth2AuthorizedClientRepository.saveAuthorizedClient(authorizedClient,principal,
-                        (HttpServletRequest) attributes.get(HttpServletRequest.class.getName()),
-                        (HttpServletResponse) attributes.get(HttpServletResponse.class.getName()));
+        OAuth2AuthorizationSuccessHandler successHandler = (authorizedClient, principal, attributes) -> {
+            oAuth2AuthorizedClientRepository
+                    .saveAuthorizedClient(authorizedClient, principal,
+                            (HttpServletRequest) attributes.get(HttpServletRequest.class.getName()),
+                            (HttpServletResponse) attributes.get(HttpServletResponse.class.getName()));
+            System.out.println("authorizedClient = " + authorizedClient);
+            System.out.println("principal = " + principal);
+            System.out.println("attributes = " + attributes);
+        };
 
         oAuth2AuthorizedClientManager.setAuthorizationSuccessHandler(successHandler);
 
         OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
 
-        if(authorizedClient != null){
 
+
+        if(authorizedClient != null){
+            OAuth2UserService<OAuth2UserRequest,OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
             ClientRegistration clientRegistration = authorizedClient.getClientRegistration();
             OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
-            OAuth2RefreshToken refreshToken = authorizedClient.getRefreshToken();
-
-            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
             OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration,accessToken);
             OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
 
             SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+            authorityMapper.setPrefix("SYSTEM_");
             Set<GrantedAuthority> grantedAuthorities = authorityMapper.mapAuthorities(oAuth2User.getAuthorities());
 
-            OAuth2AuthenticationToken authenticationToken =
-                    new OAuth2AuthenticationToken(oAuth2User,grantedAuthorities,clientRegistration.getRegistrationId());
+            OAuth2AuthenticationToken oAuth2AuthenticationToken =
+                    new OAuth2AuthenticationToken(oAuth2User,grantedAuthorities, clientRegistration.getRegistrationId());
 
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            securityContext.setAuthentication(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(oAuth2AuthenticationToken);
 
-            successHandler.onAuthorizationSuccess(authorizedClient,authenticationToken,createAttributes(request,response));
-            model.addAttribute("authentication", authenticationToken);
+            model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken);
+
         }
 
-        return "/home";
+        return "home";
     }
 
-    private Map<String, Object> createAttributes(HttpServletRequest request, HttpServletResponse response) {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put(HttpServletRequest.class.getName(), request);
-        attributes.put(HttpServletResponse.class.getName(), response);
-        return attributes;
-    }
 
     @GetMapping("/logout")
     public String logout(Authentication authentication, HttpServletResponse response, HttpServletRequest request) {
