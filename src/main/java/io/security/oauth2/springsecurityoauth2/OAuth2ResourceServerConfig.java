f077e7e3490cfc31cc76a3dd81e5554dac080e73
onjsdnjs <onjsdnjs@gmail.com>
2022-09-20 21:57:29 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
@@ -8,30 +8,36 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
-public class OAuth2ResourceServer {
+public class OAuth2ResourceServerConfig {
 
     @Autowired
     private OAuth2ResourceServerProperties properties;
 
     @Bean
-    SecurityFilterChain jwtSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(request ->request.anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
 
-    /*@Bean
-    public JwtDecoder jwtDecoder1() {
+/*    @Bean
+    public JwtDecoder jwtDecoder1(){
         return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder2(){
+        return JwtDecoders.fromOidcIssuerLocation(properties.getJwt().getIssuerUri());
     }*/
 
     @Bean
-    public JwtDecoder jwtDecoder2() {
-        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).jwsAlgorithm(SignatureAlgorithm.RS512).build();}
+    public JwtDecoder jwtDecoder3(){
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+                .jwsAlgorithm(SignatureAlgorithm.RS512).build();
+    }
 
 }
