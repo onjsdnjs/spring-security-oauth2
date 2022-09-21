905329ed78bcfeb445ba85b7e0e95d0cb95c3949
onjsdnjs <onjsdnjs@gmail.com>
2022-09-21 15:18:13 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -12,9 +12,9 @@ public class SpringSecurityOauth2Application {
     public static void main(String[] args) throws Exception {
 
 //        messageDigest("Spring Security");
-        hmac("Spring Security");
+//        hmac("Spring Security");
 //        signature("Spring Security");
-//        rsa("Spring Security");
+        rsa("Spring Security");
 
     }
 }
