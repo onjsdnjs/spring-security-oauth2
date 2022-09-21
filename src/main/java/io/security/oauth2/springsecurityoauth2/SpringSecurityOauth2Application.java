264410ecf5726f11ed138aeaa178a77deef7e4f1
onjsdnjs <onjsdnjs@gmail.com>
2022-09-21 23:37:12 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -2,16 +2,12 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static io.security.oauth2.springsecurityoauth2.MacTest.hmac;
-import static io.security.oauth2.springsecurityoauth2.RSATest.rsa;
+import static io.security.oauth2.springsecurityoauth2.JWKTest.jwk;
 
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
     public static void main(String[] args) throws Exception {
-//        messageDigest("Spring Security");
-//        signature("Spring Security");
-        hmac("Spring Security");
-        rsa("Spring Security");
+        jwk();
 //        SpringApplication.run(SpringSecurityOauth2Application.class, args);
     }
 }
