c8a3673bdf04085e7c8882d61c022a040cd85b47
onjsdnjs <onjsdnjs@gmail.com>
2022-09-23 21:43:55 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/RSATest.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/RSATest.java
@@ -19,11 +19,15 @@ public class RSATest {
         System.out.println("message : " + message);
         System.out.println("decrypted : " + decrypted);
 
+        // 키 스펙 전환하기
         byte[] bytePublicKey = publicKey.getEncoded();
         String base64PublicKey = Base64.getEncoder().encodeToString(bytePublicKey);
         byte[] bytePrivateKey = privateKey.getEncoded();
         String base64PrivateKey = Base64.getEncoder().encodeToString(bytePrivateKey);
 
+        // 키 스펙 전환하기
+
+        // X.509 표준형식
         PublicKey X509PublicKey = RSAGen.getPublicKeyFromKeySpec(base64PublicKey);
         String encrypted2 = RSAGen.encrypt(message, X509PublicKey);
         String decrypted2 = RSAGen.decrypt(encrypted2, privateKey);
@@ -31,6 +35,8 @@ public class RSATest {
         System.out.println("message : " + message);
         System.out.println("decrypted2 : " + decrypted2);
 
+
+        // PKCS8 표준형식
         PrivateKey PKCS8PrivateKey = RSAGen.getPrivateKeyFromKeySpec(base64PrivateKey);
         String decrypted3 = RSAGen.decrypt(encrypted2, PKCS8PrivateKey);
 
