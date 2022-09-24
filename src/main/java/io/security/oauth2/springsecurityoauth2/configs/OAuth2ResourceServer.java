a59541627c6a742320dc64f20689d94d835906d1
onjsdnjs <onjsdnjs@gmail.com>
2022-09-24 11:41:18 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -1,24 +1,20 @@
 package io.security.oauth2.springsecurityoauth2.configs;
 
 import io.security.oauth2.springsecurityoauth2.filter.authentication.JwtAuthenticationFilter;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Arrays;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServer {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,15 +29,12 @@ public class OAuth2ResourceServer {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService getUserDetailsService() {
+    private UserDetailsService getUserDetailsService() {
 
-        UserDetails user = User.withUsername("user").password("1234").authorities("ROLE_USER").build();
-        return new InMemoryUserDetailsManager(user);
+        User user = new User("user", "{noop}1234", Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager(user);
+
+        return userDetailsManager;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
 }
