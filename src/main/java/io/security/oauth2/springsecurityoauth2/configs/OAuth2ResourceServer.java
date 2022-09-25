ab40ab9cdfa82d3991bdcdd335321ff1fc058592
onjsdnjs <onjsdnjs@gmail.com>
2022-09-25 16:38:29 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -2,10 +2,13 @@ package io.security.oauth2.springsecurityoauth2.configs;
 
 import com.nimbusds.jose.jwk.OctetSequenceKey;
 import io.security.oauth2.springsecurityoauth2.filter.authentication.JwtAuthenticationFilter;
+import io.security.oauth2.springsecurityoauth2.filter.authorization.JwtAuthorizationMacFilter;
 import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -17,8 +20,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.Filter;
+
 @Configuration
-public class OAuth2ResourceServer {
+public class OAuth2ResourceServer{
 
     @Autowired
     private MacSecuritySigner macSecuritySigner;
@@ -32,16 +37,33 @@ public class OAuth2ResourceServer {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll().anyRequest().authenticated());
-        http.userDetailsService(getUserDetailsService());
-        http.addFilterBefore(new JwtAuthenticationFilter(http,macSecuritySigner, octetSequenceKey), UsernamePasswordAuthenticationFilter.class);
-        http.oauth2ResourceServer().jwt();
-
+        http.authorizeRequests((requests) -> requests.antMatchers("/").permitAll()
+                .anyRequest().authenticated());
+        http.userDetailsService(userDetailsService());
+        http.addFilterBefore(jwtAuthenticationFilter(macSecuritySigner, octetSequenceKey), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthorizationMacFilter(octetSequenceKey), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
-    public UserDetailsService getUserDetailsService() {
+    public JwtAuthorizationMacFilter jwtAuthorizationMacFilter(OctetSequenceKey octetSequenceKey) {
+        return new JwtAuthorizationMacFilter(octetSequenceKey);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(MacSecuritySigner macSecuritySigner, OctetSequenceKey octetSequenceKey) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(macSecuritySigner,octetSequenceKey);
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager(null));
+        return jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
 
         UserDetails user = User.withUsername("user").password("1234").authorities("ROLE_USER").build();
         return new InMemoryUserDetailsManager(user);
--- src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authentication/JwtAuthenticationFilter.java
@@ -3,36 +3,26 @@ package io.security.oauth2.springsecurityoauth2.filter.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import io.security.oauth2.springsecurityoauth2.dto.LoginDto;
-import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
 import io.security.oauth2.springsecurityoauth2.signature.SecuritySigner;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private HttpSecurity httpSecurity;
     private SecuritySigner securitySigner;
     private JWK jwk;
 
-    public JwtAuthenticationFilter(HttpSecurity httpSecurity, SecuritySigner securitySigner, JWK jwk) {
-        this.httpSecurity = httpSecurity;
+    public JwtAuthenticationFilter(SecuritySigner securitySigner, JWK jwk) {
         this.securitySigner = securitySigner;
         this.jwk = jwk;
     }
@@ -40,36 +30,34 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        AuthenticationManager authenticationManager = httpSecurity.getSharedObject(AuthenticationManager.class);
-
         ObjectMapper objectMapper = new ObjectMapper();
         LoginDto loginDto = null;
-        try {
 
+        try {
             loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-        Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
-        return authentication;
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+
     }
+
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws ServletException, IOException {
-
-        User user = (User) authResult.getPrincipal();
-
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        User user = (User)authResult.getPrincipal();
         String jwtToken;
+
         try {
             jwtToken = securitySigner.getJwtToken(user, jwk);
             response.addHeader("Authorization", "Bearer " + jwtToken);
 
         } catch (JOSEException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-
-
     }
 }
