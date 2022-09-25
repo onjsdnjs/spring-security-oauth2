f30ed5b1a0ae7d56aab63747fcf6ce152caf2bcc
onjsdnjs <onjsdnjs@gmail.com>
2022-09-25 20:22:23 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/filter/authorization/JwtAuthorizationFilter.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/filter/authorization/JwtAuthorizationFilter.java
@@ -27,11 +27,11 @@ public abstract class JwtAuthorizationFilter extends OncePerRequestFilter {
 	}
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
 		String header = request.getHeader("Authorization");
-		if (!tokenResolve(header, request, response, chain)){
-			chain.doFilter(request,response);
+		if(header == null || !header.startsWith("Bearer ")){
+			filterChain.doFilter(request,response);
 			return;
 		}
 		String token = header.replace("Bearer ", "");
@@ -63,14 +63,6 @@ public abstract class JwtAuthorizationFilter extends OncePerRequestFilter {
 			e.printStackTrace();
 		}
 
-		chain.doFilter(request, response);
+		filterChain.doFilter(request, response);
     }
-
-	private boolean tokenResolve(String header, HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-		if (header == null || !header.startsWith("Bearer ")) {
-			return false;
-		}
-		return true;
-	}
 }
