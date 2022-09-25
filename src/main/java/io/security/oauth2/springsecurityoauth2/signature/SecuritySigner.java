b96e2b17d25397cef9887ccb2b18291057ed050e
onjsdnjs <onjsdnjs@gmail.com>
2022-09-25 16:25:51 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/signature/SecuritySigner.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/signature/SecuritySigner.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
@@ -14,22 +15,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class SecuritySigner {
-    public String getJwtTokenInternal(JWSSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
-
-        List<String> authority = user.getAuthorities().stream().map(auth -> auth.getAuthority()).collect(Collectors.toList());
+    protected String getJwtTokenInternal(MACSigner jwsSigner, UserDetails user, JWK jwk) throws JOSEException {
+        JWSHeader header = new JWSHeader.Builder((JWSAlgorithm) jwk.getAlgorithm()).keyID(jwk.getKeyID()).build();
+        List<String> authorities = user.getAuthorities().stream().map(auth -> auth.getAuthority()).collect(Collectors.toList());
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject("user")
                 .issuer("http://localhost:8081")
-                .claim("username", user.getUsername())
-                .claim("authority", authority)
+                .claim("username",user.getUsername())
+                .claim("authority",authorities)
                 .expirationTime(new Date(new Date().getTime() + 60 * 1000 * 5))
                 .build();
-
-        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder((JWSAlgorithm)jwk.getAlgorithm()).keyID(jwk.getKeyID()).build(), jwtClaimsSet);
+        SignedJWT signedJWT = new SignedJWT(header,jwtClaimsSet);
         signedJWT.sign(jwsSigner);
         String jwtToken = signedJWT.serialize();
 
         return jwtToken;
     }
+
     public abstract String getJwtToken(UserDetails user, JWK jwk) throws JOSEException;
 }
