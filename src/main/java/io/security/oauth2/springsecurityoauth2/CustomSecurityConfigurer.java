e892dea566f60dd66592cb3865b55b02001e3959
onjsdnjs <onjsdnjs@gmail.com>
2022-09-27 00:55:32 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
@@ -3,26 +3,33 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-public class CustomSecurityConfigurer
-        extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+
     private boolean isSecure;
 
     @Override
     public void init(HttpSecurity builder) throws Exception {
         super.init(builder);
-        System.out.println("init method starting..");
+        System.out.println("init method started..");
     }
+
     @Override
     public void configure(HttpSecurity builder) throws Exception {
         super.configure(builder);
+        System.out.println("configure method started..");
         if(isSecure){
             System.out.println("https is required");
         }else{
             System.out.println("https is optional");
         }
-        System.out.println("configure method starting..");
     }
+
     public CustomSecurityConfigurer setFlag(boolean isSecure){
+        this.isSecure = isSecure;
+        return this;
+    }
+
+    public CustomSecurityConfigurer setFlag2(boolean isSecure){
         this.isSecure = isSecure;
         return this;
     }
--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,4 +1,3 @@
-/*
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
@@ -7,14 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer()).setFlag(true);
+        http.apply(new CustomSecurityConfigurer().setFlag(true));
         return http.build();
     }
 }
-*/
+
+
