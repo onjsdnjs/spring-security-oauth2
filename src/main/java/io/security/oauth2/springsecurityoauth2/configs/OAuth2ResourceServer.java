2b0ca04cadf9c2e26d0066f6c1e3c15f8c8002f2
onjsdnjs <onjsdnjs@gmail.com>
2022-09-28 15:53:22 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/configs/OAuth2ResourceServer.java
@@ -1,25 +1,16 @@
 package io.security.oauth2.springsecurityoauth2.configs;
 
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.crypto.RSASSAVerifier;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
 import com.nimbusds.jose.jwk.RSAKey;
 import io.security.oauth2.springsecurityoauth2.filter.authentication.JwtAuthenticationFilter;
-import io.security.oauth2.springsecurityoauth2.filter.authorization.JwtAuthorizationRsaFilter;
 import io.security.oauth2.springsecurityoauth2.filter.authorization.JwtAuthorizationRsaPublicKeyFilter;
-import io.security.oauth2.springsecurityoauth2.signature.MacSecuritySigner;
 import io.security.oauth2.springsecurityoauth2.signature.RSASecuritySigner;
 import io.security.oauth2.springsecurityoauth2.signature.RsaPublicKeySecuritySigner;
-import io.security.oauth2.springsecurityoauth2.signature.SecuritySigner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,30 +24,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class OAuth2ResourceServer {
-
-    @Autowired
-    private OAuth2ResourceServerProperties properties;
-
-    @Autowired
-    private MacSecuritySigner macSecuritySigner;
-
-    @Autowired
-    private OctetSequenceKey octetSequenceKey;
-
-    @Autowired
-    private RSASecuritySigner rsaSecuritySigner;
-
-    @Autowired
-    private RSAKey rsaKey256;
-
-    @Autowired
-    private RSAKey rsaKey512;
-
-    @Autowired
-    private RsaPublicKeySecuritySigner rsaPublicKeySecuritySigner;
-
-    @Autowired
-    private JwtDecoder jwtDecoder;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -94,8 +61,8 @@ public class OAuth2ResourceServer {
     }
 
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(RSASecuritySigner rsaSecuritySigner, RSAKey rsaKey256) throws Exception {
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(rsaSecuritySigner,rsaKey256);
+    public JwtAuthenticationFilter jwtAuthenticationFilter(RsaPublicKeySecuritySigner rsaPublicKeySecuritySigner, RSAKey rsaKey) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(rsaPublicKeySecuritySigner,rsaKey);
         jwtAuthenticationFilter.setAuthenticationManager(authenticationManager(null));
         return jwtAuthenticationFilter;
     }
@@ -112,33 +79,4 @@ public class OAuth2ResourceServer {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    private SecuritySigner securitySigner() {
-        if (isAlg("RS256")){
-            return rsaPublicKeySecuritySigner;
-
-        }else if(isAlg("RS512")){
-            return rsaSecuritySigner;
-
-        }else if(isAlg("HS256")){
-            return macSecuritySigner;
-        }
-        return null;
-    }
-    private JWK jwk() {
-
-        if(isAlg("RS256")){
-            return rsaKey256;
-
-        }else if(isAlg("RS512")){
-            return rsaKey512;
-
-        }else if(isAlg("HS256")){
-            return octetSequenceKey;
-        }
-        return null;
-    }
-
-    private boolean isAlg(String alg) {
-        return properties.getJwt().getJwsAlgorithms().get(0).equals(alg);
-    }
 }
