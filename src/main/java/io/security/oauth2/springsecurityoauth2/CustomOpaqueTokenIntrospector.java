b297526bbfe87e9ca1221055dbda39a6c78a31ce
onjsdnjs <onjsdnjs@gmail.com>
2022-09-30 00:57:11 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/CustomOpaqueTokenIntrospector.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomOpaqueTokenIntrospector.java
@@ -1,5 +1,7 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal;
@@ -13,22 +15,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CustomOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
-    private OpaqueTokenIntrospector delegate = new NimbusOpaqueTokenIntrospector(
-            "http://localhost:8080/realms/oauth2/protocol/openid-connect/token/introspect"
-            , "oauth2-client-app"
-            , "CQueEWXZYmv7IIZVxbvh2uwxptXVaRcX");
 
+    private OpaqueTokenIntrospector delegate;
+
+    public CustomOpaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
+        delegate = new NimbusOpaqueTokenIntrospector(
+                properties.getOpaquetoken().getIntrospectionUri(),
+                properties.getOpaquetoken().getClientId(),
+                properties.getOpaquetoken().getClientSecret());
+    }
+
+    @Override
     public OAuth2AuthenticatedPrincipal introspect(String token) {
-        OAuth2AuthenticatedPrincipal principal = this.delegate.introspect(token);
-        return new DefaultOAuth2AuthenticatedPrincipal(
-                principal.getName(), principal.getAttributes(), extractAuthorities(principal));
+
+        OAuth2AuthenticatedPrincipal principal = delegate.introspect(token);
+        return new DefaultOAuth2AuthenticatedPrincipal(principal.getName(),
+                principal.getAttributes(),
+                extractAuthorities(principal));
     }
 
     private Collection<GrantedAuthority> extractAuthorities(OAuth2AuthenticatedPrincipal principal) {
+
         List<String> scopes = principal.getAttribute(OAuth2TokenIntrospectionClaimNames.SCOPE);
-        return scopes.stream().map(scope -> "ROLE_"+scope.toUpperCase())
+        return scopes.stream().map(scope -> "ROLE_" + scope.toUpperCase())
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
     }
-
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
@@ -3,31 +3,45 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AnonymousAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration(proxyBeanMethods = false)
+import java.util.List;
+
+@Configuration
 public class OAuth2ResourceServer {
 
     @Bean
-    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests(
                 (requests) -> requests.anyRequest().authenticated());
-        http.oauth2ResourceServer().opaqueToken();
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::opaqueToken);
         return http.build();
     }
 
     /*@Bean
-    public OpaqueTokenIntrospector nimbusOpaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
+    public OpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties){
         OAuth2ResourceServerProperties.Opaquetoken opaquetoken = properties.getOpaquetoken();
-        return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(),opaquetoken.getClientId(),opaquetoken.getClientSecret());
+        return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(),
+                opaquetoken.getClientId(),
+                opaquetoken.getClientSecret());
     }*/
 
     @Bean
-    public OpaqueTokenIntrospector introspector() {
-        return new CustomOpaqueTokenIntrospector();
+    public OpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties){
+        return new CustomOpaqueTokenIntrospector(properties);
     }
 }
