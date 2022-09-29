8386438a8e454c234e0f1def0f5fa28ee1ee3835
onjsdnjs <onjsdnjs@gmail.com>
2022-09-29 21:45:36 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServer.java
@@ -20,14 +20,10 @@ public class OAuth2ResourceServer {
         return http.build();
     }
 
-    /*@Bean
+    @Bean
     public OpaqueTokenIntrospector nimbusOpaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
         OAuth2ResourceServerProperties.Opaquetoken opaquetoken = properties.getOpaquetoken();
         return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(),opaquetoken.getClientId(),opaquetoken.getClientSecret());
-    }*/
-
-    @Bean
-    public OpaqueTokenIntrospector introspector() {
-        return new CustomOpaqueTokenIntrospector();
     }
+
 }
