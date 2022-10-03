7b9bd8c23aec851d7b8a55506df1ff127e006aee
onjsdnjs <onjsdnjs@gmail.com>
2022-10-03 10:05:02 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/DefaultSecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/DefaultSecurityConfig.java
@@ -4,33 +4,35 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
 
 	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests(authorizeRequests ->
-				authorizeRequests.anyRequest().authenticated()
-			)
-			.formLogin(withDefaults());
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests(request -> request.anyRequest().authenticated());
+		http.formLogin();
+
 		return http.build();
 	}
 
 	@Bean
-	public UserDetailsService getUserDetailsService() {
-
-		User user = new User("user", "{noop}1234", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+	public UserDetailsService userDetailsService(){
+		UserDetails user = User.withUsername("user").password("{noop}1234").authorities("ROLE_USER").build();
 		return new InMemoryUserDetailsManager(user);
 	}
+
+	@Bean
+	public OAuth2AuthorizationService oAuth2AuthorizationService(){
+		return new InMemoryOAuth2AuthorizationService();
+	}
+
 }
