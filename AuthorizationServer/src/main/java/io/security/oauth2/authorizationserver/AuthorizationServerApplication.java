1eee872fc93f8e8d1468f9c48d4bdbaf98871d1a
onjsdnjs <onjsdnjs@gmail.com>
2022-10-11 21:10:52 +0900

    lecture

---
+++ AuthorizationServer/src/main/java/io/security/oauth2/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,11 @@
+package io.security.oauth2.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/ResourceServerApplication.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/ResourceServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityOauth2Application {
+public class ResourceServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityOauth2Application.class, args);
+        SpringApplication.run(ResourceServerApplication.class, args);
     }
 }
