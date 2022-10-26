edf662e1d5d4e06f931989b9136de39b1e67527e
onjsdnjs <onjsdnjs@gmail.com>
2022-10-26 17:26:51 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/model/users/social/OAuth2ProviderUser.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/model/users/social/OAuth2ProviderUser.java
@@ -42,7 +42,8 @@ public abstract class OAuth2ProviderUser implements ProviderUser {
 
     @Override
     public List<? extends GrantedAuthority> getAuthorities() {
-        return oAuth2User.getAuthorities().stream().map(authority -> new SimpleGrantedAuthority(authority.getAuthority())).collect(Collectors.toList());
+        return oAuth2User.getAuthorities().stream().map(authority ->
+                new SimpleGrantedAuthority(authority.getAuthority())).collect(Collectors.toList());
     }
 
     @Override
