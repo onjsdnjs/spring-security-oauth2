91ed6cb8c7717926fb949a0c99f2b2bf39ee1db6
onjsdnjs <onjsdnjs@gmail.com>
2022-10-26 09:22:19 +0900

    lecture

--- src/main/java/io/security/oauth2/springsecurityoauth2/common/enums/OAuth2Config.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/common/enums/OAuth2Config.java
@@ -1,10 +1,7 @@
 package io.security.oauth2.springsecurityoauth2.common.enums;
 
 public class OAuth2Config {
-
     public enum SocialType{
-
-        GOOGLE,NAVER,KAKAO,APPLE,FACEBOOK
-
+        GOOGLE,APPLE,FACEBOOK,NAVER,KAKAO,
     }
 }
